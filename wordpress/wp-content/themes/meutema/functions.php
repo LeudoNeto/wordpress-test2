d898348c25611aefc6494a22cb2282ca84f635db
Leudo Neto <leudoneto1234@gmail.com>
2022-07-16 17:49:37 -0300

    outras páginas

--- wordpress/wp-content/themes/meutema/functions.php
+++ wordpress/wp-content/themes/meutema/functions.php
@@ -1,3 +1,45 @@
+<style>
+
+.styled-table {
+    border-collapse: collapse;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1em;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 70em;
+}
+
+.styled-table thead tr {
+    background-color: black;
+    color: #ffffff;
+    text-align: left;
+}
+
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid black;
+}
+
+.styled-table tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
+}
+
+</style>
+
 <?php
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
@@ -7,6 +49,72 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version') // this only works if you have Version in the style header
     );
 }
+
+function mostrar_competidores($atts)
+{
+
+    global $wpdb;
+    $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}competidores ORDER BY name", ARRAY_A );
+
+    $tabela = '<table class="styled-table">
+                <thead>
+                    <tr style = "text-align: left;">
+                        <th>Nome</th>
+                        <th>Imagem</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody><br><br>';
+
+    foreach ($results as $line)
+    {
+        $line_id = $line["id"];
+        $tabela .= "<tr>";
+        $tabela .= "<td>".$line["name"]."</td>";
+        $tabela .= "<td><a href='imagem_dos_competidores/$line_id.png'>ver foto</a></td>";
+        $tabela .= "<td>".$line["points"]."</td>";
+        $tabela .= "</tr>";
+    }
+
+    $tabela .= "</tbody>
+              </table>";
+
+    return $tabela;
+}
+add_shortcode('competidores','mostrar_competidores');
+
+function mostrar_ranking($atts)
+{
+
+    global $wpdb;
+    $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}competidores ORDER BY name", ARRAY_A );
+
+    $tabela = '<table class="styled-table">
+                <thead>
+                    <tr style = "text-align: left;">
+                        <th>Nome</th>
+                        <th>Imagem</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+    foreach ($results as $line)
+    {
+        $line_id = $line["id"];
+        $tabela .= "<tr>";
+        $tabela .= "<td>".$line["name"]."</td>";
+        $tabela .= "<td><a href='imagem_dos_competidores/$line_id.png'>ver foto</a></td>";
+        $tabela .= "<td>".$line["points"]."</td>";
+        $tabela .= "</tr>";
+    }
+
+    $tabela .= "</tbody>
+              </table>";
+
+    return $tabela;
+}
+add_shortcode('ranking','mostrar_ranking');
 
 
 ?>
--- wordpress/wp-content/themes/twentytwentytwo/functions.php
+++ wordpress/wp-content/themes/twentytwentytwo/functions.php
@@ -1,3 +1,45 @@
+<style>
+
+.styled-table {
+    border-collapse: collapse;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1em;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 70em;
+}
+
+.styled-table thead tr {
+    background-color: black;
+    color: #ffffff;
+    text-align: left;
+}
+
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid black;
+}
+
+.styled-table tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
+}
+
+</style>
+
 <?php
 /**
  * Twenty Twenty-Two functions and definitions
@@ -65,3 +107,107 @@ add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
 
 // Add block patterns
 require get_template_directory() . '/inc/block-patterns.php';
+
+
+function mostrar_competidores($atts)
+{
+
+    global $wpdb;
+    $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}competidores ORDER BY name", ARRAY_A );
+
+    $tabela = '<table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Imagem</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+    foreach ($results as $line)
+    {
+        $line_id = $line["id"];
+        $tabela .= "<tr>";
+        $tabela .= "<td>".$line["name"]."</td>";
+        $tabela .= "<td><a href='/wordpress/imagem_dos_competidores/$line_id.png'>ver foto</a></td>";
+        $tabela .= "<td>".$line["points"]."</td>";
+        $tabela .= "</tr>";
+    }
+
+    $tabela .= "</tbody>
+              </table>";
+
+    return $tabela;
+}
+add_shortcode('competidores','mostrar_competidores');
+
+function mostrar_ranking($atts)
+{
+
+    global $wpdb;
+    $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}competidores ORDER BY points DESC", ARRAY_A );
+
+    $tabela = '<table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>Posição</th>
+                        <th>Nome</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+	$i = 1;
+    foreach ($results as $line)
+    {
+        $tabela .= "<tr>";
+		$tabela .= "<td>$i"."º</td>";
+        $tabela .= "<td>".$line["name"]."</td>";
+        $tabela .= "<td>".$line["points"]."</td>";
+        $tabela .= "</tr>";
+
+		$i++;
+    }
+
+    $tabela .= "</tbody>
+              </table>";
+
+    return $tabela;
+}
+add_shortcode('ranking','mostrar_ranking');
+
+function detalhes_dos_competidores($atts)
+{
+
+    global $wpdb;
+    $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}competidores ORDER BY name", ARRAY_A );
+
+    $tabela = '<table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+						<th>Foto</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+    foreach ($results as $line)
+    {
+		$line_id = $line['id'];
+        $tabela .= "<tr>";
+        $tabela .= "<td>".$line["name"]."</td>";
+		$tabela .= "<td>".$line['descricao']."</td>";
+		$tabela .= "<td>"."<img src='/wordpress/imagem_dos_competidores/$line_id.png' height='80' >"."</td>";
+        $tabela .= "<td>".$line["points"]."</td>";
+        $tabela .= "</tr>";
+    }
+
+    $tabela .= "</tbody>
+              </table>";
+
+    return $tabela;
+}
+add_shortcode('detalhes','detalhes_dos_competidores');
